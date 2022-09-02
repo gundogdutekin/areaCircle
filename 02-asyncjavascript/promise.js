da27e5f60c93a810f0369fa6a494c761a152343b
tekin <gundogdutekin@gmail.com>
2022-09-02 22:30:16 +0300

    Javascript promise nesnesi kullanımı

---
+++ 02-asyncjavascript/promise.js
@@ -0,0 +1,26 @@
+const books = [
+    { name: 'Kitap 1', author: 'Yazar 1' },
+    { name: 'Kitap 2', author: 'Yazar 2' },
+    { name: 'Kitap 3', author: 'Yazar 3' },
+]
+
+const addBook = (newBook) => {
+    const promise1 = new Promise((resolve, reject) => {
+        books.push(newBook)
+        resolve('Başarılı.Kitap  eklendi.')
+        reject('HATA! Kitap eklenemedi')
+    })
+    return promise1
+}
+addBook({ name: 'Kitap 4', author: 'Yazar 4' })
+    .then((value) => {
+        books.map((book) => {
+            console.log(
+                `Kitap adı  : ${book.name} ---->  Yazar : ${book.author} -----> `,
+                value
+            )
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
