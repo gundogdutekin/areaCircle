dbc7c6520295588c6cf4d19c0f7965d996badf54
tekin <gundogdutekin@gmail.com>
2022-09-04 22:11:54 +0300

    NodeJs ile sunucuyu ayağa kaldırma

--- 07-server-create/index.js
+++ 07-server-create/index.js
@@ -4,22 +4,22 @@ const server = http.createServer((req, res) => {
     const url = req.url
     if (url === '/') {
         res.writeHeader('200', { 'Content-Type': 'text/html; charset: utf-8' })
-        res.write('<h1>ANASAYFA</h1>')
+        res.write('<h1>ANASAYFA SAYFASINA HOSGELDINIZ</h1>')
     } else if (url === '/about') {
         res.writeHeader('200', { 'Content-Type': 'text/html; charset: utf-8' })
-        res.write('<h1>HAKKIMIZDA SAYFASI</h1>')
+        res.write('<h1>HAKKIMIZDA SAYFASINA HOSGELDINIZ</h1>')
     } else if (url === '/contact') {
         res.writeHeader('200', { 'Content-Type': 'text/html; charset: utf-8' })
-        res.write('<h1>İLETİŞİM SAYFASI</h1> ')
+        res.write('<h1>ILETISIM SAYFASINA HOSGELDINIZ</h1> ')
     } else {
         res.writeHeader('404', { 'Content-Type': 'text/html; charset: utf-8' })
-        res.write('<h1>404 aradığınız sayfa bulunmadı</h1> ')
+        res.write('<h1>404 aradiginiz sayfa bulunmadi</h1> ')
     }
 
     res.end()
 })
 
-const port = 3000
+const port = 5000
 
 server.listen(port, () => {
     console.log(`Sunucu ${port} nda başlatıldı `)
