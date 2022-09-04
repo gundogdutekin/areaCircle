0c076fb5d611a739950e7c1dd91271c9c3dfab0d
tekin <gundogdutekin@gmail.com>
2022-09-05 00:49:08 +0300

    ExperssJs e giriş

---
+++ 08-express-giris/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.status(200).send('INDEX SAYFASI')
+})
+app.get('/about', (req, res) => {
+    res.status(200).send('ABOUT SAYFASI')
+})
+app.get('/contact', (req, res) => {
+    res.status(200).send('CONTACT SAYFASI')
+})
+app.get('*', (req, res) => {
+    res.status(404).send('404 ! ARADIĞINIZ SAYFA BULUNAMADI')
+})
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda çalıştırıldı.`)
+})
